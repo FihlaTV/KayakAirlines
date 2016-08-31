1940f409dd26b2706cdea66bd76cade0e763fc95
Oguz Babaoglu <oguz.babaoglu@gmail.com>
2016-08-31 14:30:09 +0200

    Remove redundant fab

--- app/src/main/java/com/oguzbabaoglu/kayakairlines/features/airlinelist/AirlineListActivity.java
+++ app/src/main/java/com/oguzbabaoglu/kayakairlines/features/airlinelist/AirlineListActivity.java
@@ -1,9 +1,7 @@
 package com.oguzbabaoglu.kayakairlines.features.airlinelist;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -14,7 +12,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.oguzbabaoglu.kayakairlines.R;
 import com.oguzbabaoglu.kayakairlines.util.Lambda;
@@ -53,16 +50,7 @@ public class AirlineListActivity extends AppCompatActivity
     });
 
     airlineTabLayout.setupWithViewPager(airlineViewPager);
-
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
-
+    
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
         this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
