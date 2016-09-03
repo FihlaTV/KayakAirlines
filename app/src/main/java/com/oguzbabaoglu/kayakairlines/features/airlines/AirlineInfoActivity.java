03ab28eb0243d73e1a365309b3448856b9ea7e35
Oguz Babaoglu <oguz.babaoglu@gmail.com>
2016-09-03 14:42:31 +0200

    Add "not implemented" to navigation

--- app/src/main/java/com/oguzbabaoglu/kayakairlines/features/airlines/AirlineInfoActivity.java
+++ app/src/main/java/com/oguzbabaoglu/kayakairlines/features/airlines/AirlineInfoActivity.java
@@ -2,6 +2,7 @@ package com.oguzbabaoglu.kayakairlines.features.airlines;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,7 +12,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 
 import com.oguzbabaoglu.kayakairlines.R;
 import com.oguzbabaoglu.kayakairlines.domain.Airline;
@@ -25,13 +25,14 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AirlineInfoActivity extends AppCompatActivity
-    implements NavigationView.OnNavigationItemSelectedListener, AirlineInfoView {
+public class AirlineInfoActivity extends AppCompatActivity implements AirlineInfoView {
 
   private static final int INDEX_ALL = 0;
   private static final int TAB_COUNT = 2;
 
   @BindView(R.id.toolbar) Toolbar toolbar;
+  @BindView(R.id.drawer_layout) DrawerLayout drawerLayout;
+  @BindView(R.id.nav_view) NavigationView navigationView;
   @BindView(R.id.airline_list_viewpager) ViewPager airlineViewPager;
   @BindView(R.id.airline_list_tablayout) TabLayout airlineTabLayout;
 
@@ -44,14 +45,14 @@ public class AirlineInfoActivity extends AppCompatActivity
     setSupportActionBar(toolbar);
     Dagger.INJECTOR.airlineComponent().inject(this);
 
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-    drawer.addDrawerListener(toggle);
+        this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawerLayout.addDrawerListener(toggle);
     toggle.syncState();
-
-    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-    navigationView.setNavigationItemSelectedListener(this);
+    navigationView.setNavigationItemSelectedListener(item -> {
+      Snackbar.make(navigationView, "Not implemented.", Snackbar.LENGTH_SHORT).show();
+      return true;
+    });
 
     presenter.setView(this);
     presenter.init();
@@ -64,31 +65,6 @@ public class AirlineInfoActivity extends AppCompatActivity
     } else {
       super.onBackPressed();
     }
-  }
-
-  @SuppressWarnings("StatementWithEmptyBody")
-  @Override
-  public boolean onNavigationItemSelected(MenuItem item) {
-    // Handle navigation view item clicks here.
-    int id = item.getItemId();
-
-    if (id == R.id.nav_camera) {
-      // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
-
-    } else if (id == R.id.nav_slideshow) {
-
-    } else if (id == R.id.nav_manage) {
-
-    } else if (id == R.id.nav_share) {
-
-    } else if (id == R.id.nav_send) {
-
-    }
-
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-    drawer.closeDrawer(GravityCompat.START);
-    return true;
   }
 
   @Override public void displayListTabs(List<Airline> all, List<Airline> starred) {
